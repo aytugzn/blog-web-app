3daf0d326552d1ef4c32e72f0f4ed3f0511365a8
aytugzn <uzunaytug3@gmail.com>
2025-07-18 02:09:08 +0300

    Refactor backend structure and apply small UI adjustments

--- index.js
+++ index.js
@@ -1,2 +1,77 @@
 import express from "express";
 import bodyParser from "body-parser";
+
+const app = express();
+const port = 4000;
+
+let posts = [
+  { id: 1, title: "İlk Post", content: "Merhaba dünya!", date: new Date()},
+];
+let postId = posts.length;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//*ALL POSTS AT START
+
+app.get("/posts", (req,res)=>{
+    res.status(200).json(posts);
+});
+
+//*POST BY ID
+
+app.get("/post/:id",(req,res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=> post.id===id);
+    if (!post) return res.status(404).json({message: "Post not found"});
+    res.status(200).json(post);
+});
+
+//*CREATE NEW POST
+
+app.post("/posts",(req,res)=>{
+    let newId = postId + 1;
+    const data = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        date: new Date()
+    }
+    postId = newId;
+    posts.push(data);
+    res.status(201).json(posts);
+});
+
+//*PATCH POST BY ID
+
+app.patch("/posts/:id",(req,res)=>{
+const id = parseInt(req.params.id);
+const post = posts.find((post)=> post.id===id);
+console.log(post);
+if (!post) return res.status(404).json({ message: "Post not found" });
+
+if(req.body.title) post.title=req.body.title;
+if(req.body.content) post.content=req.body.content;
+res.json(post);
+});
+
+app.get("/api/posts/:id", (req,res)=>{
+const id = parseInt(req.params.id);
+const post = posts.find((post)=> post.id===id);
+if (!post) return res.status(404).json({ message: "Post not found" });
+res.json(post);
+});
+
+//*DELETE POST BY ID
+
+app.delete("/posts/:id",(req,res)=>{
+    const id = parseInt(req.params.id);
+    const postIndex = posts.findIndex((post)=> post.id===id);
+    if(postIndex===-1) return res.status(404).json({message: "Post not found"});
+    posts.splice(postIndex,1);
+    res.status(200).json({message: "Post deleted"});
+});
+
+app.listen(port, ()=>{
+    console.log(`API server is running on port ${port}`);
+});
--- server.js
+++ server.js
@@ -4,6 +4,91 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "http://localhost:4000";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+
+//*ALL POSTS AT START
+
+app.get("/", async (req,res)=>{
+try {
+    console.log(API_URL)
+const response = await axios.get(`${API_URL}/posts`);
+console.log(response.data);
+res.render("index", {posts: response.data});
+} catch (error) {
+    console.log(error.response.data);
+    res.status(500).json({message: "Error fetching posts"});
+}
+});
+
+//*POST BY ID
+
+app.get("/post/:id", async (req,res)=>{
+try {
+    const id = parseInt(req.params.id);
+    const response = await axios.get(`${API_URL}/post/${id}`)
+    res.render("index", {post: response.data});
+} catch (error) {
+    console.log(error.response.data);
+    res.json({message: "Post not found"});
+}
+});
+
+//*CREATE NEW POST
+
+app.get("/new", (req,res)=>{
+    res.render("modify",{heading: "New Post", submit: "Create Post"});
+});
+
+app.post("/api/posts", async (req,res)=>{
+    try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+        console.log(response.data);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500).json({message: `Error creating post`})
+    }
+});
+
+//*PATCH POST BY ID
+app.get("/edit/:id", async (req,res)=>{
+    const response = await axios.get(`${API_URL}/api/posts/${parseInt(req.params.id)}`);
+    console.log(response.data);
+    res.render("modify",{heading: "Edit Post", submit: "Update Post", post:response.data});
+});
+
+app.post("/api/posts/:id", async (req,res)=>{
+    console.log("called");
+  console.log(req.body);
+try {
+    const id = parseInt(req.params.id);
+    const response = await axios.patch(`${API_URL}/posts/${id}`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+} catch (error) {
+    console.log(error.response.data);
+    res.status(500).json({message: "Error updating post"})
+}
+});
+
+//*DELETE POST BY ID
+
+app.get("/api/posts/delete/:id", async (req,res)=>{
+    try {
+        const id = parseInt(req.params.id);
+        const response = await axios.delete(`${API_URL}/posts/${id}`);
+        console.log(response.data)
+            res.redirect("/");
+    } catch (error) {
+        res.status(500).json({message: "Error deleting post"});
+    }
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
